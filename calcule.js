4653f131f6155dcf52a1742d4d3226cee7eb53a0
paulo <paulofrontend@gmail.com>
2017-10-16 19:54:57 -0200

    adjust in console names

--- calcule.js
+++ calcule.js
@@ -5,11 +5,11 @@
   const valueMax = getArray.reduce((a,b)=> Math.max(a,b));
   const valueMin = getArray.reduce((a,b)=> Math.min(a,b));
   const size = arr.length;
-  const media = getArray.reduce((a,b)=> a + b / size);
+  const average = getArray.reduce((a,b)=> a + b / size);
 
-  console.log(valueMax);
-  console.log(valueMax);
-  console.log(size);
-  console.log(media);
+  console.log(`Value maximum ${valueMax}`);
+  console.log(`Value minimum ${valueMin}`);
+  console.log(`Length of array ${size}`);
+  console.log(`Average ${average}`);
   
 })();
